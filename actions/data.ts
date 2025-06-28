433a929a8c74feeda87d16a442c95852ae77260e
Muhammad Farhan <farhan_75230@hotmail.com>
2025-06-28 13:57:06 +0200

    form input added!

--- actions/data.ts
+++ actions/data.ts
@@ -17,3 +17,24 @@ export const fetchTasks = async () => {
     data,
   };
 };
+
+export const createTask = async (formData: FormData) => {
+  const title = formData.get("title") as string;
+  const body = formData.get("body") as string;
+  const author = formData.get("author") as string;
+  const cover = formData.get("cover_url") as string;
+
+  const supabase = await createSupabaseServerSide();
+  // const { data, error } = await supabase
+  //   .from("blogs")
+  //   .insert([{ title, body, author, cover_url }]);
+  // // .select();
+
+  const { data, error } = await supabase
+    .from("blogs")
+    .insert([{ title: title, body: body, author: author, cover_url: cover }])
+    .select();
+
+  console.log({ title, body, author, cover });
+  revalidatePath("/");
+};
--- app/(Creation)/create/page.tsx
+++ app/(Creation)/create/page.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import { subscribeFormData } from "@/actions/addform";
+import { createTask } from "@/actions/data";
 
 export default function page() {
   return (
     <div>
       <p className="text-center">Create a new blog Entry here.</p>
       <h1>Blog Form</h1>
-      <form action={subscribeFormData}>
-        <input name="id" type="text" placeholder="Insert your id" />
+      <form action={createTask}>
+        {/* <input name="id" type="text" placeholder="Insert your id" /> */}
         <input name="title" type="text" placeholder="Insert your title" />
-        <input name="author" type="text" placeholder="Insert your author" />
         <input name="body" type="text" placeholder="Insert your body" />
+        <input name="author" type="text" placeholder="Insert your author" />
+        <input name="cover_url" type="text" placeholder="Insert your url" />
+        {/* <input name="created_at" type="text" placeholder="Insert your author" /> */}
         <button type="submit">Submit</button>
       </form>
     </div>
